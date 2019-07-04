faed39c0a1980d168f76a52005b407d1caaa6808
Jochem Smit <j.h.smit@rug.nl>
2019-07-04 19:46:13 +0200

    multi dilate and erode

--- smitsuite/image_processing.py
+++ smitsuite/image_processing.py
@@ -1,7 +1,23 @@
 import mahotas as mh
 from scipy.signal import medfilt
+import mahotas as mh
 import pybromo
 
+
+def multi_erode(img, x):
+    img = img.astype(bool)
+    for i in range(x):
+        img = mh.erode(img)
+
+    return img
+
+
+def multi_dilate(img, x):
+    img = img.astype(bool)
+    for i in range(x):
+        img = mh.dilate(img)
+
+    return img
 
 
 def generate_background(image, median_kernel=11, gaussian_kernel=11, dtype=int):
