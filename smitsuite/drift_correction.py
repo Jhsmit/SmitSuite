19d974fbcb053ef0a4ea38fbb0c344a8e0f36f0a
Jochem Smit <j.h.smit@rug.nl>
2019-07-04 19:45:44 +0200

    updates to drift correction

--- smitsuite/drift_correction.py
+++ smitsuite/drift_correction.py
@@ -11,8 +11,87 @@ from symfit import Parameter, Variable, Fit
 import argparse
 import sys
 
-#http://scikit-image.org/docs/dev/auto_examples/transform/plot_register_translation.html
+
 class DriftCorrect(object):
+    def __init__(self, data_arr):
+        self.data_arr = data_arr
+
+    def correct(self, step_size=1, interpolation='poly', upsample_factor=100):
+        self.calc_drift(step_size=step_size, interpolation=interpolation, upsample_factor=upsample_factor)
+        self.apply_shifts()
+
+        return self.corrected
+
+    def calc_drift(self, step_size=1, interpolation='poly', upsample_factor=100):
+        f = np.arange(len(self.data_arr))
+
+        data = self.data_arr[::step_size]
+        shifts = list(self.gen_drift_shift(data, upsample_factor=upsample_factor))
+        self.x_shift, self.y_shift = np.array(shifts).T
+        indices = np.arange(0, len(self.data_arr), step_size)
+
+        if interpolation == 'poly':
+            model = self._get_poly(6)
+            x_result = Fit(model, indices, self.x_shift).execute()
+            y_result = Fit(model, indices, self.y_shift).execute()
+            self.x = model(f, **x_result.params)
+            self.y = model(f, **y_result.params)
+        #
+        # if 'deg' in interpolation:
+        #     pass
+        elif interpolation == 'linear':
+            #todo always include last frame
+            self.x = np.interp(f, indices, self.x_shift)
+            self.y = np.interp(f, indices, self.y_shift)
+
+        return self.x, self.y
+
+    def apply_shifts(self):
+        #todo rename x, s_shift also y
+        self.corrected = np.array([frame for frame in self._gen_corrected_im(self.x, self.y, self.data_arr)])
+
+    @staticmethod
+    def _gen_corrected_im(x, y, data):
+        for i, (xs, ys, frame) in enumerate(zip(x, y, data)):
+            corrected = scipy_shift(frame, (xs, ys))
+            printProgress(i, len(data))
+            yield corrected
+
+    def plot_shifts(self):
+        #todo catch no drift calculated exception
+        f = np.arange(len(self.data_arr))
+
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        ax.plot(f, self.x)
+        ax.plot(f, self.y)
+        plt.xlabel('Frame number')
+        plt.ylabel('Shift')
+        plt.show()
+
+    @staticmethod
+    def _get_poly(degree):
+        x = Variable(name='x')
+        params = [Parameter(name='a' + str(i)) for i in range(degree + 1)]
+        p = params[0]
+        for i in np.arange(1, degree + 1):
+            p += params[i] * x ** i
+
+        return p
+
+    @staticmethod
+    def gen_drift_shift(data_arr, upsample_factor=100):
+        ref_frame = data_arr[0]
+        yield (0, 0) # Yield shift for first frame
+        for i, frame in enumerate(data_arr[1:]):
+            shift, error, diffphase = register_translation(ref_frame, frame, upsample_factor=upsample_factor)
+
+            printProgress(i, len(data_arr) - 1)
+            yield shift
+
+
+#http://scikit-image.org/docs/dev/auto_examples/transform/plot_register_translation.html
+class DriftCorrectFile(object):
     def __init__(self, file_path):
         self.file_path = file_path
         print(self.file_path)
