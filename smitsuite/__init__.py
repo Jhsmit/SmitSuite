e2d42c8eeab00d4c92fc0ad0102e043188063a9b
Jochem Smit <j.h.smit@rug.nl>
2019-07-04 19:46:23 +0200

    drift correct import

--- smitsuite/__init__.py
+++ smitsuite/__init__.py
@@ -3,5 +3,5 @@ from .PALM_combine import stack_all
 from .traces import find_peaks, get_traces, trace_export
 from .image_processing import generate_background
 from .autocorrelation import acorrelate
-from .drift_correction import drift_correct
+from .drift_correction import drift_correct, DriftCorrect
 from .fitgauss import fit_gaussian, gaussian
